436a698d957c6068d10069d854a4357740fb32a7
hybelius <hybelius@student.chalmers.se>
2020-02-26 14:44:38 +0100

    Kollektiv cleanup

--- src/mcmc.py
+++ src/mcmc.py
@@ -5,10 +5,11 @@ import copy
 from .toric_model import Toric_code
 from .util import Action
 
-rule_table = np.array(([[0, 1, 2, 3], [1, 0, 3, 2], [2, 3, 0, 1], [3, 2, 1, 0]]), dtype=int)    # Identity = 0
-                                                                                                # pauli_x = 1
-                                                                                                # pauli_y = 2
-                                                                                                # pauli_z = 3
+rule_table = np.array(([[0, 1, 2, 3], [1, 0, 3, 2], 
+                        [2, 3, 0, 1], [3, 2, 1, 0]]), dtype=int)    # Identity = 0
+                                                                    # pauli_x = 1
+                                                                    # pauli_y = 2
+                                                                    # pauli_z = 3
 
 
 class Chain:
@@ -18,94 +19,66 @@ class Chain:
         self.p = p
         self.p_logical = 0
 
-    def permute_error(self):  # eventually rewrite to remove middle steps.
-        self.toric.qubit_matrix = permute_error(self.toric.qubit_matrix, self.size, self.p, self.p_logical)
+    def update_chain(self):
+        if np.random.rand() < self.p_logical:
+            new_matrix = apply_random_logical(self.toric.qubit_matrix)
+        else:
+            new_matrix = apply_random_stabilizer(self.toric.qubit_matrix)
+
+        #r = r_chain(self.toric.qubit_matrix, new_matrix, self.p)
+
+        qubit_errors_current = np.count_nonzero(self.toric.qubit_matrix)
+        qubit_errors_new = np.count_nonzero(new_matrix)
+
+        r = ((self.p / 3.0) / (1.0 - self.p)) ** (qubit_errors_new - qubit_errors_current)
+
+        if np.random.rand() < r:
+            self.toric.qubit_matrix = new_matrix
 
     def plot(self, name):
+        self.toric.syndrom('next_state')
         self.toric.plot_toric_code(self.toric.next_state, name)
-
-    def set_p_logical(self, p_logical):
-        self.p_logical = p_logical
-
-    def get_p(self):
-        return self.p
-
-    def get_qubit_matrix(self):
-        return self.toric.qubit_matrix
-
-    def get_toric(self):
-        return self.toric
-
-    def set_toric(self, new_toric):
-        # self.toric = copy.deepcopy(new_toric) # tror inte att man behöver en deepcopy här, men har kvar som kommentar om det krågnlar?
-        self.toric = new_toric
 
 
 def r_flip(chain_lo, chain_hi):
-    p_lo = chain_lo.get_p()
-    p_hi = chain_hi.get_p()
+    p_lo = chain_lo.p
+    p_hi = chain_hi.p
 
-    ne_lo = np.count_nonzero(chain_lo.get_qubit_matrix())
-    ne_hi = np.count_nonzero(chain_hi.get_qubit_matrix())
+    ne_lo = np.count_nonzero(chain_lo.toric.qubit_matrix)
+    ne_hi = np.count_nonzero(chain_hi.toric.qubit_matrix)
     # compute eqn (5) in high threshold paper
     r = ((p_lo / p_hi) * ((1 - p_hi) / (1 - p_lo))) ** (ne_hi - ne_lo)
 
-    if r > 1:
-        # flip them if r > 1
-        temp = chain_lo.get_toric()
-        chain_lo.set_toric(chain_hi.get_toric())
-        chain_hi.set_toric(temp)
-    elif rand.random() < r:
+    if rand.random() < r:
         # flip them with prob r if r < 1
-        temp = chain_lo.get_toric()
-        chain_lo.set_toric(chain_hi.get_toric())
-        chain_hi.set_toric(temp)
+        temp = chain_lo.toric
+        chain_lo.toric = chain_hi.toric
+        chain_hi.toric = temp
 
 
-'''
-def apply_stabilizer(toric_model, row=int, col=int, operator=int):
-    # operator is 1 (X <-> vertex) or 3 (Z <-> plaquette)
-    d = toric_model.system_size #input np.array of form (qubit_matrix=int, row=int, col=int, add_operator=int)
-
-    if operator == 1:
-        action1 = Action(position = np.array([1, row, col]), action = operator)
-        action2 = Action(position = np.array([1, row, (col-1)%d]), action = operator)
-        action3 = Action(position = np.array([0, row, col]), action = operator)
-        action4 = Action(position = np.array([0, (row-1)%d, col]), action = operator)
-    elif operator == 3:
-        action1 = Action(position = np.array([1, row, col]), action = operator)
-        action2 = Action(position = np.array([0, row, col]), action = operator)
-        action3 = Action(position = np.array([0, row, (col+1)%d]), action = operator)
-        action4 = Action(position = np.array([1, (row+1)%(d), col]), action = operator)
-
-    toric_model.step(action1)
-    toric_model.step(action2)
-    toric_model.step(action3)
-    toric_model.step(action4)
-'''
-
-
-def apply_random_logical(qubit_matrix, size=int):
+def apply_random_logical(qubit_matrix):
+    size = qubit_matrix.shape[1]
     operator = np.random.randint(1, 4)  # operator to use, 2 (Y) will make both X and Z on the same layer
     orientation = np.random.randint(0, 2)  # 0 - horizontal, 1 - vertical
 
     if orientation == 0:  # Horizontal
         if operator == 2:
             order = np.random.randint(0, 2)  # make sure that we randomize which operator goes verically and horizontally
-            temp_qubit_matrix = apply_logical_horizontal(qubit_matrix, size, np.random.randint(size), (order * 2 - 1) % 4)
-            return apply_logical_horizontal(temp_qubit_matrix, size, np.random.randint(size), (order * 2 + 1) % 4)
+            temp_qubit_matrix = apply_logical_horizontal(qubit_matrix, np.random.randint(size), (order * 2 - 1) % 4)
+            return apply_logical_horizontal(temp_qubit_matrix, np.random.randint(size), (order * 2 + 1) % 4)
         else:
-            return apply_logical_horizontal(qubit_matrix, size, np.random.randint(size), operator)
+            return apply_logical_horizontal(qubit_matrix, np.random.randint(size), operator)
     elif orientation == 1:  # Vertical
         if operator == 2:
             order = np.random.randint(0, 2)  # make sure that we randomize which operator goes verically and horizontally
-            temp_qubit_matrix = apply_logical_vertical(qubit_matrix, size, np.random.randint(size), (order * 2 - 1) % 4)
-            return apply_logical_vertical(temp_qubit_matrix, size, np.random.randint(size), (order * 2 + 1) % 4)
+            temp_qubit_matrix = apply_logical_vertical(qubit_matrix, np.random.randint(size), (order * 2 - 1) % 4)
+            return apply_logical_vertical(temp_qubit_matrix, np.random.randint(size), (order * 2 + 1) % 4)
         else:
-            return apply_logical_vertical(qubit_matrix, size, np.random.randint(size), operator)
+            return apply_logical_vertical(qubit_matrix, np.random.randint(size), operator)
 
 
-def apply_logical_vertical(qubit_matrix, size=int, col=int, operator=int):  # col goes from 0 to size-1, operator is either 1 or 3, corresponding to x and z
+def apply_logical_vertical(qubit_matrix, col=int, operator=int):  # col goes from 0 to size-1, operator is either 1 or 3, corresponding to x and z
+    size = qubit_matrix.shape[1]
     if operator == 1:  # makes sure the logical operator is applied on the correct layer, so that no syndromes are generated
         layer = 1
     else:
@@ -118,13 +91,14 @@ def apply_logical_vertical(qubit_matrix, size=int, col=int, operator=int):  # co
     old_operators = qubit_matrix[qubit_matrix_layers, rows, cols]
     new_operators = rule_table[operator][old_operators]
 
-    result_qubit_matrix = np.copy(qubit_matrix)
+    result_qubit_matrix = qubit_matrix
     result_qubit_matrix[qubit_matrix_layers, rows, cols] = new_operators
 
     return result_qubit_matrix
 
 
-def apply_logical_horizontal(qubit_matrix, size=int, row=int, operator=int):  # col goes from 0 to size-1, operator is either 1 or 3, corresponding to x and z
+def apply_logical_horizontal(qubit_matrix, row=int, operator=int):  # col goes from 0 to size-1, operator is either 1 or 3, corresponding to x and z
+    size = qubit_matrix.shape[1]
     if operator == 1:
         layer = 0
     else:
@@ -137,15 +111,16 @@ def apply_logical_horizontal(qubit_matrix, size=int, row=int, operator=int):  # 
     old_operators = qubit_matrix[qubit_matrix_layers, rows, cols]
     new_operators = rule_table[operator][old_operators]
 
-    result_qubit_matrix = np.copy(qubit_matrix)
+    result_qubit_matrix = qubit_matrix
     result_qubit_matrix[qubit_matrix_layers, rows, cols] = new_operators
 
     return result_qubit_matrix
 
 
-def test_apply_stabilizer(qubit_matrix, size=int, row=int, col=int, operator=int):
+def apply_stabilizer(qubit_matrix, row=int, col=int, operator=int):
     # gives the resulting qubit error matrix from applying (row, col, operator) stabilizer
     # doesn't update input qubit_matrix
+    size = qubit_matrix.shape[1]
     if operator == 1:
         qubit_matrix_layers = np.array([1, 1, 0, 0])
         rows = np.array([row, row, row, (row - 1) % size])
@@ -161,110 +136,70 @@ def test_apply_stabilizer(qubit_matrix, size=int, row=int, col=int, operator=int
 
     result_qubit_matrix = np.copy(qubit_matrix)
     result_qubit_matrix[qubit_matrix_layers, rows, cols] = new_operators
-
     return result_qubit_matrix
 
 
-def test_apply_random_stabilizer(qubit_matrix, size):
+def apply_random_stabilizer(qubit_matrix):
     # select random coordinates where to apply operator
+    size = qubit_matrix.shape[1]
     row = np.random.randint(0, size)  # gives int in [0, d-1]
     col = np.random.randint(0, size)
     operator = np.random.randint(0, 2)  # we only care about X and Z, and Y is represented by 2. Therefore:
     if operator == 0:
         operator = 3
-    return test_apply_stabilizer(qubit_matrix, size, row, col, operator)
+    return apply_stabilizer(qubit_matrix, row, col, operator)
 
 
-def apply_random_stabilizer(toric):
-    toric.qubit_matrix = test_apply_random_stabilizer(toric.qubit_matrix, toric.system_size)
+def apply_stabilizers_uniform(qubit_matrix, p=0.5):
+    size = qubit_matrix.shape[1]
+    result_qubit_matrix = np.copy(qubit_matrix)
+    random_stabilizers = np.random.rand(2, size, size)
+    random_stabilizers = np.less(random_stabilizers, p) 
+    
+    # Numpy magic for iterating through matrix
+    it = np.nditer(random_stabilizers, flags=['multi_index'])
+    while not it.finished:
+        if it[0]:
+            op, row, col = it.multi_index
+            if op == 0:
+                op = 3
+            result_qubit_matrix = apply_stabilizer(result_qubit_matrix, row, col, op)
+        it.iternext()
+    return result_qubit_matrix
 
 
-def apply_n_independent_random_stabilizers(toric, n=int):
-    for i in range(0, n):
-        apply_random_stabilizer(toric)
-
-
-def apply_n_distinct_random_stabilizers(toric, n=int):
-    stabilizer_array = np.zeros(2 * toric.system_size**2)
-    stabilizer_array[:n] = np.ones(n, dtype=bool)
-    np.random.shuffle(stabilizer_array)
-
-    stabilizer_matrix = stabilizer_array.reshape(toric.system_size, toric.system_size, 2)
-    for index, stab in np.ndenumerate(stabilizer_matrix):
-        if stab:
-            operator = index[2]
-            if operator == 0:
-                operator = 3
-            toric.qubit_matrix = test_apply_stabilizer(toric.qubit_matrix, toric.system_size, index[0], index[1], operator)
-
-
-def error_ratio(qubit_matrix_current, qubit_matrix_next, p=float):
-    qubit_errors_current = np.count_nonzero(qubit_matrix_current)
-    qubit_errors_new = np.count_nonzero(qubit_matrix_next)
-
-    ratio = ((p / 3.0) / (1.0 - p)) ** (qubit_errors_new - qubit_errors_current)
-    return ratio
-
-
-def permute_error(qubit_matrix, size, p, p_logical):
-    if np.random.rand() < p_logical:
-        new_matrix = apply_random_logical(qubit_matrix, size)
-    else:
-        new_matrix = test_apply_random_stabilizer(qubit_matrix, size)
-
-    r = error_ratio(qubit_matrix, new_matrix, p)
-    if np.random.rand() < r:
-        return new_matrix
-    else:
-        return qubit_matrix
-
-
-def init_error(toric, qubit_matrix):
-    toric.qubit_matrix = np.copy(qubit_matrix)
-    toric.syndrom('next_state')
-
-
-def define_equivalence_class(qubit_matrix,size):
-#checks odd and even errors in each layer
-#gives a combination of four numbers corresponding to an equivalence class
+def define_equivalence_class(qubit_matrix):
+    #checks odd and even errors in each layer
+    #gives a combination of four numbers corresponding to an equivalence class
 
     #checks odd or even x-errors in first layer
-    x1prov=np.count_nonzero(qubit_matrix[0]==1)
-    #checks odd or even z-errors in first layer
-    z1prov=np.count_nonzero(qubit_matrix[0]==3)
-    #checks odd or even y-erros in first layer and adds them to total number of x/z errors in first layer
-    y1prov=np.count_nonzero(qubit_matrix[0]==2)
-    x1=x1prov+y1prov
-    z1=z1prov+y1prov
+    x1prov = np.count_nonzero(qubit_matrix[0] == 1)
 
+    #checks odd or even z-errors in first layer
+    z1prov = np.count_nonzero(qubit_matrix[0] == 3)
+
+    #checks odd or even y-erros in first layer and adds them to total number of x/z errors in first layer
+    y1prov = np.count_nonzero(qubit_matrix[0] == 2)
+    x1 = x1prov + y1prov
+    z1 = z1prov + y1prov
 
     #checks odd or even x-errors in second layer
-    x2prov=np.count_nonzero(qubit_matrix[1]==1)
+    x2prov = np.count_nonzero(qubit_matrix[1] == 1)
+
     #checks odd or even z-errors in second layer
-    z2prov=np.count_nonzero(qubit_matrix[1]==3)
+    z2prov = np.count_nonzero(qubit_matrix[1] == 3)
+
     #checks odd or even y-erros in second layer and adds them to total number of x/z errors in second layer
-    y2prov=np.count_nonzero(qubit_matrix[1]==2)
-    x2=x2prov+y2prov
-    z2=z2prov+y2prov
+    y2prov = np.count_nonzero(qubit_matrix[1] == 2)
+    x2 = x2prov + y2prov
+    z2 = z2prov + y2prov
+
     # stores whether there was an odd or even number of errors
-    if x1 % 2 == 0:
-        x1=0
-    else:
-        x1=1
-    
-    if z1 % 2 == 0:
-        z1=0
-    else:
-        z1=1
-    
-    if x2 % 2 == 0:
-        x2=0
-    else:
-        x2=1
-    if z2 % 2 == 0:
-        z2=0
-    else:
-        z2=1
-    Enumb=x1+z1*2+x2*4+z2*8
-    return Enumb
+    x1 = x1 % 2
+    z1 = z1 % 2
+
+    x2 = x2 % 2
+    z2 = z2 % 2
+
+    return x1 + z1 * 2 + x2 * 4 + z2 * 8
     
--- test_mcmc.py
+++ test_mcmc.py
@@ -4,42 +4,6 @@ import numpy as np
 import copy
 
 def main():
-    size = 3
-    toric = Toric_code(size)
-
-    init_errors = np.array([[1, 0, 2, 1], [1, 0, 1, 1], [1, 0, 3, 1], [[1, 0, 4, 1]], [[1, 0, 0, 1]]])
-    for arr in init_errors:
-        print(arr)
-        action = Action(position = arr[:3], action = arr[3])
-        toric.step(action)
-
-    toric.plot_toric_code(toric.next_state, 'Chain_0')
-
-    apply_n_independent_random_stabilizers(toric, 1000)
-
-    qubit_matrix = np.copy(toric.qubit_matrix)
-    p = 0.1
-    p_logical = 0.5
-
-    #for i in range(100):
-    #    qubit_matrix = permute_error(qubit_matrix, size, p)
-
-    res = input('plot? ')
-    while not res == 'n':
-        try:
-            n = int(res)
-        except:
-            n = 1
-        for i in range(n):
-            qubit_matrix = permute_error(qubit_matrix, size, p, p_logical)
-            toric.qubit_matrix = np.copy(qubit_matrix)
-        toric.plot_toric_code(toric.next_state, 'Chain_0')
-        res = input('plot? ')
-    #print("vertex: " + str(toric.next_state[0, :, :]))
-    #print("plaquette: " + str(toric.next_state[1, :, :])) 
-
-
-def main2():
     size = 5
     toric_init = Toric_code(size)
 
@@ -62,7 +26,7 @@ def main2():
     for i in range(N):
         ladder.append(Chain(size, p_start + ((p_end - p_start) / (N - 1)) * i))
         ladder[i].toric = copy.deepcopy(toric_init)  # give all the same initial state
-    ladder[N - 1].set_p_logical(0.5)  # set top chain as the only on where logicals happen
+    ladder[N - 1].p_logical = 0.5  # set top chain as the only on where logicals happen
 
     steps = input('How many steps? Ans: ')
     iters = input('How many iterations for each step? Ans: ')
@@ -79,7 +43,7 @@ def main2():
             # run mcmc for each chain [steps] times
             for i in range(N):
                 for _ in range(steps):
-                    ladder[i].permute_error()
+                    ladder[i].update_chain()
             # now attempt flips from the top down
             for j in reversed(range(N - 1)):
                 r_flip(ladder[j], ladder[j + 1])
@@ -92,12 +56,5 @@ def main2():
         iters = input('How many iterations for each step? Ans: ')
 
 
-'''
-    rule_table = np.array(([[0,1,2,3],[1,0,3,2],[2,3,0,1],[3,2,1,0]]), dtype=int)
-    opps = np.array([0, 1, 2])
-    rules = rule_table[1][opps]
-    print(rules)
-'''
-
 if __name__ == "__main__":
-    main2()
+    main()
