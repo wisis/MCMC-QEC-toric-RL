1bca1c69815521457618013751eb6fcde0d48505
hybelius <hybelius@student.chalmers.se>
2020-03-03 09:25:31 +0100

    la till kod som håller reda på antalet i varje ekvivalensklass

--- test_mcmc.py
+++ test_mcmc.py
@@ -7,26 +7,33 @@ def main():
     size = 5
     toric_init = Toric_code(size)
 
+    '''
     # Initial error configuration
-    init_errors = np.array([[1, 0, 1, 1], [1, 2, 1, 1], [1, 3, 1, 1], [1, 4, 1, 1], [1, 1, 1, 1]])
+    init_errors = np.array([[1, 0, 1, 3], [1, 1, 0, 1], [1, 3, 3, 3], [1, 4, 1, 1], [1, 1, 1, 1]])
     for arr in init_errors:  # apply the initial error configuration
         print(arr)
         action = Action(position=arr[:3], action=arr[3])
         toric_init.step(action)
-    # plot initial error configuration
-    toric_init.plot_toric_code(toric_init.next_state, 'Chain_init')
-
+    '''
     # create the diffrent chains in an array
-    N = 9  # number of chains in ladder
+    N = 15  # number of chains in ladder
     ladder = []  # ladder to store all chains
     p_start = 0.1  # what should this really be???
     p_end = 0.75  # p at top chain as per high-threshold paper
 
+    # test random error initialisation
+    toric_init.generate_random_error(p_start)
+    toric_init.qubit_matrix = apply_stabilizers_uniform(toric_init.qubit_matrix)
+
+    # plot initial error configuration
+    toric_init.plot_toric_code(toric_init.next_state, 'Chain_init')
+
     # add and copy state for all chains in ladder
     for i in range(N):
-        ladder.append(Chain(size, p_start + ((p_end - p_start) / (N - 1)) * i))
+        p_i = p_start + ((p_end - p_start) / (N - 1)) * i
+        ladder.append(Chain(size, p_i))
         ladder[i].toric = copy.deepcopy(toric_init)  # give all the same initial state
-    ladder[N - 1].p_logical = 0.5  # set top chain as the only on where logicals happen
+    ladder[N - 1].p_logical = 0.5  # set top chain as the only one where logicals happen
 
     steps = input('How many steps? Ans: ')
     iters = input('How many iterations for each step? Ans: ')
@@ -39,18 +46,30 @@ def main():
             steps = 1
             iters = 1
 
-        for _ in range(iters):
+        error_samples = np.zeros(iters, dtype=int)
+
+        for j in range(iters):
             # run mcmc for each chain [steps] times
             for i in range(N):
                 for _ in range(steps):
                     ladder[i].update_chain()
             # now attempt flips from the top down
-            for j in reversed(range(N - 1)):
-                r_flip(ladder[j], ladder[j + 1])
+            for i in reversed(range(N - 1)):
+                r_flip(ladder[i], ladder[i + 1])
+            # record current equivalence class in bottom layer
+            error_samples[j] = define_equivalence_class(ladder[0].toric.qubit_matrix)
 
         # plot all chains
         for i in range(N):
             ladder[i].plot('Chain_' + str(i))
+
+        # count number of occurrences of each equivalence class
+        # equivalence_class_count[i] is the number of occurences of equivalence class number 'i'
+        # if
+        equivalence_class_count = np.bincount(error_samples, minlength=15)
+
+        print('Equivalence classes: \n', np.arange(16))
+        print('Count: \n', equivalence_class_count)
 
         steps = input('How many steps? Ans: ')
         iters = input('How many iterations for each step? Ans: ')
