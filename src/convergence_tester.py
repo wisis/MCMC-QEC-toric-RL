9293f999c9a9641b2f366776439b1496311f4251
hybelius <hybelius@student.chalmers.se>
2020-05-27 15:24:08 +0200

    Ändrade hur vissa saker beräknas

--- src/convergence_tester.py
+++ src/convergence_tester.py
@@ -107,17 +107,19 @@ def Nc_visuals(files=6, SEQ=20):
     # Number of samples per (SEQ, tol) pair
     pop = int(tot_pts / Nc_pts)
 
+    stats['steps'].replace(-1, 1e6, inplace=True)
+
     for Nc in Nc_values:
         # Window of points for current Nc value
-        window = stats[stats['Nc'] == Nc]
+        window = stats[stats['Nc'] == Nc].copy()
         # Only look at converged runs
-        window = window[window['steps'] != -1]
+        #window['steps'] = window['steps'].replace(-1, 1e6)
         # Calculate step time
         window['steptime'] = window['time'] / window['steps']
         # Mean and std values over converged runs
         agg = window.agg(['mean', 'std', geom_mean, geom_std])
         # number of converged runs
-        nbr_converged = window.shape[0]
+        nbr_converged = window[window['steps'] != 1e6].shape[0]
         # temporary dict to append to aggregated data
         tmp_dict = {'Nc': Nc, 'nbr_converged': nbr_converged}
         for name in ['time', 'steps', 'steptime']:
@@ -130,9 +132,9 @@ def Nc_visuals(files=6, SEQ=20):
     #stats.boxplot(column='time', by='Nc', return_type='both')
 
     time = agg_data['time_mean'].to_numpy()
-    time_err = agg_data['time_std']
+    time_err = agg_data['time_std'] / np.sqrt(pop)
 
-    left = 0.13
+    left = 0.12
     plt.rcParams.update({'font.size': 48, 'figure.subplot.top': 0.91, 'figure.subplot.bottom': 0.18, 'figure.subplot.left': left, 'figure.subplot.right': 1 - left})
     plt.rc('axes', labelsize=60)#, titlesize=40) 
 
@@ -140,14 +142,15 @@ def Nc_visuals(files=6, SEQ=20):
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.errorbar(Nc_values, time, yerr=time_err, fmt='o--', ms=24, lw=8, capsize=16, capthick=8, c=c)#, label='Convergence time')
+    ax.errorbar(Nc_values, time, yerr=time_err, fmt='-', ms=24, lw=6, capsize=12, capthick=6, c=c)#, label='Convergence time')
 
     pad = 20.0
     ax.set_xlabel('\\texttt{Nc}', labelpad=pad)
     ax.set_ylabel('Konvergenstid [s]', labelpad=pad)
-    ax.set_ylim([-25, 170])
+    ax.set_ylim([0, 110])
     ax.set_xlim([2, 32])
     ax.set_title('SEQ = {}'.format(SEQ), pad=pad)
+    ax.grid(True, axis='both')
     #ax.legend(loc='upper right')
     #plt.savefig('plots/Nc_data_SEQ{}.png'.format(SEQ))
     plt.show()
@@ -193,7 +196,8 @@ def convergence_tester(file_path):
     crits_stats.to_pickle(file_path)
 
 
-def conv_test_visuals(files=50):
+def conv_test_visuals():
+    files = 100
     file_base = 'output/conv_data_'
     stats = pd.DataFrame(columns=['SEQ', 'eps', 'kld', 'tvd', 'steps'])
     for i in range(files):
@@ -206,6 +210,9 @@ def conv_test_visuals(files=50):
 
     SEQ_values = np.unique(stats['SEQ'].to_numpy())
     eps_values = np.unique(stats['eps'].to_numpy())
+
+    def weight(tvd, steps):
+        return tvd
 
     # Number of data points
     tot_pts = stats.shape[0]
@@ -230,6 +237,9 @@ def conv_test_visuals(files=50):
             for name in ['kld', 'tvd', 'steps']:
                 for op in ['mean', 'std', 'geom_mean', 'geom_std', 'max', 'min']:
                     tmp_dict[name + '_' + op] = agg.loc[op][name]
+
+            tmp_dict['tvd_steps_outer'] = weight(tmp_dict['tvd_mean'], tmp_dict['steps_mean'])
+            tmp_dict['tvd_steps_inner'] = weight(window['tvd'], window['steps']).mean()
 
             agg_data = agg_data.append(tmp_dict, ignore_index=True)
         
@@ -264,8 +274,12 @@ def conv_test_visuals(files=50):
         tvds = window['tvd_mean']
         tvd_stds = window['tvd_std']
 
+        steps = window['steps_mean']
+
         tvd_min = window['tvd_min']
         tvd_max = window['tvd_max']
+
+        y = window['tvd_steps_inner']
 
         #row = i // plot_cols
         #col = i % plot_cols
@@ -276,7 +290,8 @@ def conv_test_visuals(files=50):
         #ax = host[i]
         ls = linestyles[i]
         #ax.errorbar(epss, tvds, yerr=tvd_stds, label='Distance')
-        ax.plot(epss, tvds, label='SEQ = {}'.format(SEQ), lw = 4, ls = ls, c = c)
+        ax.plot(epss, y, label='SEQ = {}'.format(SEQ), lw = 4, ls = ls, c = c)
+        #ax.set_yscale('log')
         #ax.plot(epss, tvd_min, label='Min distance')
         #ax.plot(epss, tvd_max, label='Max distance')
 
@@ -287,9 +302,10 @@ def conv_test_visuals(files=50):
         ax.set_ylabel('Maximal distans', fontsize=big_font, labelpad=pad)
         #ax.legend(loc='upper left')
         #ax.set_yscale('log')
-        ax.set_ylim(0.01, 0.025)
+        #ax.set_ylim(0.01, 0.025)
         #ax.set_yscale(scale)
         #ax.set_xlim(min(epss) * 0.7, max(epss) * 1.05)
+        ax.grid(True, axis='both')
         ax.tick_params(axis='both', which='major', labelsize=small_font)
         ax.ticklabel_format(style='sci', axis='y', scilimits=(-2, -2))
         ax.ticklabel_format(style='sci', axis='x', scilimits=(-3, -3))
@@ -310,6 +326,16 @@ def conv_test_visuals(files=50):
     #fig.suptitle('Total variational distance from converged distribution')
     #plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
     #plt.set_cmap('hot') 
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(111)
+
+    Y = SEQ_values
+    X = eps_values
+    Z = np.reshape(agg_data['tvd_steps_inner'].to_numpy(), (eps_pts, SEQ_pts))
+
+    cont = ax2.contourf(X, Y, Z, cmap='cubehelix')
+    plt.colorbar(cont, ax=ax2)
+
     plt.show()
 
 
@@ -356,6 +382,66 @@ def conv_stats(file_path, p_error, SEQ=30, eps=0.006):
             crits_stats.to_pickle(file_path)
 
     crits_stats.to_pickle(file_path)
+
+
+def conv_stats_visuals(files=50, SEQ=30, eps=0.006, p=0.1):
+    
+    end = 0.06 if p == 0.1 else 0.08
+
+    file_base = 'output/conv_stats_' + 'SEQ{}_eps{:.3f}_p{:.3f}_'.format(SEQ, eps, p)
+    stats = pd.DataFrame(columns=['kld', 'tvd', 'steps', 'success', 'distr'])
+    for i in range(files):
+        filename = (file_base + str(i)).replace('0.', '0')  + '.xz'
+        df = pd.read_pickle(filename)
+        stats = pd.concat([stats, df])
+
+    # remove non converged runs
+    converged = stats[stats['steps'] != -1]
+    
+    y = converged['tvd']
+
+    y = y[y != 0]
+    #y = y[y > 0]
+    #y = np.log(y)
+    #print(y[(y > 0.0099) & (y < 0.01)])
+
+    converged = converged[converged['tvd'] != 0]
+    success_rate = converged['success'].mean()
+    distrs = converged['distr'].to_numpy()
+    p_max_mean = np.mean([np.max(distr) for distr in distrs])
+    nbr_converged = converged.shape[0]
+    tot_pts = stats.shape[0]
+
+    perc95 = np.percentile(y, 95)
+    print('95th percentile:', perc95)
+    print('Success rate:', success_rate)
+    print('Mean of p_max:', p_max_mean)
+    print('Total runs:', tot_pts)
+    print('Converged runs:', nbr_converged)
+
+    small_font = 48
+    mid_font = 60
+
+    left = 0.12
+    plt.rcParams.update({'figure.subplot.top': 0.98, 'figure.subplot.bottom': 0.16, 'figure.subplot.left': left, 'figure.subplot.right': 1 - left})
+
+    c = plt.cm.cubehelix(0.2)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.hist(y, bins=100, density=True, range=(0, end), color=c)
+    ax.axvline(x = perc95, ls='--', lw=8, c='k', label=('$P_{95}$ = ' + '{:.3f}'.format(perc95)))
+    
+    pad = 20.0
+    ax.set_xlabel('Maximal distans', fontsize = mid_font, labelpad = pad)
+    ax.set_ylabel('Antal', fontsize = mid_font, labelpad = pad)
+    ax.tick_params(axis='both', which='major', labelsize=small_font)
+    ax.ticklabel_format(style='sci', axis='x', scilimits=(-2, -2))
+    ax.xaxis.offsetText.set_fontsize(small_font)
+    ax.legend(loc = 'upper right', fontsize = mid_font, handlelength=1)
+    ax.grid(True, axis='both')
+
+    plt.show()
 
 
 def bias_tester(file_path, p_error, SEQ=30, eps=0.006):
@@ -411,62 +497,47 @@ def bias_tester(file_path, p_error, SEQ=30, eps=0.006):
     crits_stats.to_pickle(file_path)
 
 
-def conv_stats_visuals(files=50, SEQ=30, eps=0.006, p=0.1):
+def bias_stats(p):
+    p_str = 'p{:.3f}'.format(p).replace('.', '')
+    print(p_str)
 
-    end = 0.06 if p == 0.1 else 0.08
+    df = pd.DataFrame()
 
-    file_base = 'output/conv_stats_' + 'SEQ{}_eps{:.3f}_p{:.3f}_'.format(SEQ, eps, p)
-    stats = pd.DataFrame(columns=['kld', 'tvd', 'steps', 'success', 'distr'])
-    for i in range(files):
-        filename = (file_base + str(i)).replace('0.', '0')  + '.xz'
-        df = pd.read_pickle(filename)
-        stats = pd.concat([stats, df])
+    drop = ['kld', 'tvd', 'steps', 'success', 'distr']
 
-    # remove non converged runs
-    converged = stats[stats['steps'] != -1]
+    for file in os.listdir('output/'):
+        if file.startswith('bias') and p_str in file:
+            tmp = pd.read_pickle('output/' + file)
+            tmp.drop(drop, axis=1, inplace=True)
+            df = pd.concat([df, tmp], ignore_index=True)
     
-    y = converged['tvd']
+    for init in ['seed', 'morph']:
 
-    y = y[y != 0]
-    #y = y[y > 0]
-    #y = np.log(y)
-    #print(y[(y > 0.0099) & (y < 0.01)])
+        z = 1.96 # 95% confidence
 
-    converged = converged[converged['tvd'] != 0]
-    success_rate = converged['success'].mean()
-    distrs = converged['distr'].to_numpy()
-    p_max_mean = np.mean([np.max(distr) for distr in distrs])
-    nbr_converged = converged.shape[0]
-    tot_pts = stats.shape[0]
+        conv = df.copy()  #[df['steps_' + init] != -1]
+        conv['steps_' + init].replace(-1.0, 1e6, inplace=True)
+        print('Converged {}: {}'.format(init, df[df['steps_' + init] != -1].shape[0]))
 
-    perc95 = np.percentile(y, 95)
-    print('95th percentile:', perc95)
-    print('Success rate:', success_rate)
-    print('Mean of p_max:', p_max_mean)
-    print('Total runs:', tot_pts)
-    print('Converged runs:', nbr_converged)
+        max_p = conv['distr_' + init].apply(lambda x: np.max(x))
+        mean_max_p = max_p.mean()
+        std_max_p = max_p.std()
+        conf_max_p = std_max_p / np.sqrt(conv.shape[0]) * z
+        print('Mean max p {}: {:.4f} +- {:.4f}'.format(init, mean_max_p, conf_max_p))
 
-    small_font = 48
-    mid_font = 60
+        success = (conv['class_seed'] == conv['distr_' + init].apply(lambda x: np.argmax(x)))
+        P_s = success.mean()
+        std_P_s = success.std()
+        #conf_P_s = z * np.sqrt( (P_s * (1 - P_s)) / conv.shape[0] )
+        conf_P_s = std_P_s / np.sqrt(conv.shape[0]) * z
+        print('Success rate {}: {} +- {:.4f}'.format(init, P_s, conf_P_s))
+        #print(std_P_s / np.sqrt(conv.shape[0]) * z)
 
-    left = 0.12
-    plt.rcParams.update({'figure.subplot.top': 0.98, 'figure.subplot.bottom': 0.16, 'figure.subplot.left': left, 'figure.subplot.right': 1 - left})
+        mean_steps = conv['steps_' + init].mean()
+        std_steps = conv['steps_' + init].std()
+        print('Mean steps {}: {:.0f} +- {:.0f}'.format(init, mean_steps, std_steps))    
 
-    c = plt.cm.cubehelix(0.2)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.hist(y, bins=100, density=True, range=(0, end), color=c)
-    ax.axvline(x = perc95, ls='--', lw=8, c='k', label=('$P_{95}$ = ' + '{:.3f}'.format(perc95)))
-    
-    pad = 20.0
-    ax.set_xlabel('Maximal distans', fontsize = mid_font, labelpad = pad)
-    ax.set_ylabel('Antal', fontsize = mid_font, labelpad = pad)
-    ax.tick_params(axis='both', which='major', labelsize=small_font)
-    ax.ticklabel_format(style='sci', axis='x', scilimits=(-2, -2))
-    ax.xaxis.offsetText.set_fontsize(small_font)
-    ax.legend(loc = 'upper right', fontsize = mid_font, handlelength=1)
-
+    print('Non converged both: {}'.format(df[(df['steps_seed'] == -1) & (df['steps_morph'] == -1)].shape[0]))
     plt.show()
 
 
@@ -786,9 +857,11 @@ if __name__ == '__main__':
 
     #file_path = os.path.join(local_dir, 'conv_stats_SEQ{}_eps{:.3f}_' + 'p{:.3f}_{}'.format(p_error, array_id))
     #conv_stats(file_path, p_error)
-    file_path = os.path.join(local_dir, 'bias_test_SEQ{}_eps{:.3f}_' + 'p{:.3f}_{}'.format(p_error, array_id))
-    bias_tester(file_path, p_error)
+    #file_path = os.path.join(local_dir, 'bias_test_SEQ{}_eps{:.3f}_' + 'p{:.3f}_{}'.format(p_error, array_id))
+    #bias_tester(file_path, p_error)
 
     #Nc_visuals(files = 6, SEQ = 8)
-    #conv_test_visuals()
-    #conv_stats_visuals(p=0.1)
+    conv_test_visuals()
+    #conv_stats_visuals(p=0.185)
+
+    #bias_stats(p=0.1)
